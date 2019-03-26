5ed7c25da68ac1083080563783d5cb9112751723
Aldhair <u201315853@upc.edu.pe>
2019-03-26 02:36:47 -0500

    Basic 2

--- Basics2.go
+++ Basics2.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"math"
+	"runtime"
 	"strconv"
+	"time"
 )
 
 func TutorialText(tutorialName string, theme string) {
@@ -65,4 +67,57 @@ func main() {
 	)
 	fmt.Println(" ")
 
+	//Switch
+	fmt.Print("Go runs on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		//freebsd, openbsd
+		//plan9,windows ...
+		fmt.Printf("%s. \n", os)
+	}
+	fmt.Println(" ")
+
+	//
+	fmt.Println("When's Saturday?")
+	today := time.Now().Weekday()
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today.")
+	case today + 1:
+		fmt.Println("Tomorrow.")
+	case today + 2:
+		fmt.Println("In two days.")
+	default:
+		fmt.Println("Too far away.")
+	}
+	fmt.Println(" ")
+
+	//
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good morning!")
+	case t.Hour() < 17:
+		fmt.Println("Good Afternoon.")
+	default:
+		fmt.Println("Good evening.")
+	}
+	fmt.Println(" ")
+
+	//Defer
+	defer fmt.Println("world!")
+	fmt.Println("hello")
+	fmt.Println(" ")
+
+	//Stacking Defer
+	fmt.Println("counting")
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+	fmt.Println("done")
+	fmt.Println(" ")
 }
